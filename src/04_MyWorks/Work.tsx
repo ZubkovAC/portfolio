4b6d180650525fde7eda07903c124fa0f8e4b9df
ZubkovAC <3y6kob90@gmail.com>
2021-02-20 10:01:20 +0300

    first commit

--- src/04_MyWorks/Work.tsx
+++ src/04_MyWorks/Work.tsx
@@ -11,7 +11,6 @@ export const Work=(props:WorkPropsType)=>{
     return(
         <div className={css.work}>
             <a href='https://res.cloudinary.com/time2hack/image/upload/q_auto:good/5-best-javascript-editors-social-md.jpg'><img src={img} alt="photo+work"/></a>
-
                 <h4 style={{backgroundColor:'maroon'}}>{props.title}</h4>
                 <p>{props.message}</p>
         </div>
