21a5eaec4c570401553d6b59cc129a5dd7ba3a0a
ZubkovAC <3y6kob90@gmail.com>
2021-05-05 03:29:15 +0300

    porfolio-03-04

--- src/02_Face/Face.tsx
+++ src/02_Face/Face.tsx
@@ -14,14 +14,10 @@ export const Face=()=>{
                        <span className={css.Test3}> A.C.</span>
                     </span>
                     </h3>
-                    <h4 className={css.h4_text} style={{margin:'0px'}}>(Upgrade in progress.)</h4>
-                    <h4 className={css.h4_text}>
-                        Welcome to Portfolio Zybkov A.C.
-                        <br/>
-                        next Upgrade to 5.05.21
-                        <br/>
-                        site v.2 site is not optimized for 4k+
-                    </h4>
+                    {/*<h4 className={css.h4_text} style={{margin:'0px'}}>(Upgrade in progress.)</h4>*/}
+                    {/*<h4 className={css.h4_text}>*/}
+                    {/*    Welcome to Portfolio Zybkov A.C.*/}
+                    {/*</h4>*/}
                 </div >
 
             </div>
--- src/05_Contact/Сommunication.tsx
+++ src/05_Contact/Сommunication.tsx
@@ -1,7 +1,8 @@
 import css from './Contact.module.scss'
 import emailjs from 'emailjs-com';
 import {useState} from "react";
-import  './../components/Input/Input.module.scss'
+import './../components/Input/Input.module.scss'
+
 type ContactInputType = {
     id: number
     title: string
@@ -20,45 +21,44 @@ const ContactInput: ContactInputType[] = [
 
 export const Communication = () => {
 
-    const [edit,setEdit]=useState<boolean>(true)
+    const [edit, setEdit] = useState<boolean>(true)
 
-    const [name,setName]=useState<string>('')
-    const [email,setEmail]=useState<string>('')
-    const [phone,setPhone]=useState<any>(null)
-    const [message,setMessage]=useState<string>('')
+    const [name, setName] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+    const [phone, setPhone] = useState<any>(null)
+    const [message, setMessage] = useState<string>('')
 
-    const testName =(value:string) => {
+    const testName = (value: string) => {
         setName(value)
         testButton()
     }
-    const testEmail =(value:string) => {
+    const testEmail = (value: string) => {
         setEmail(value)
         testButton()
     }
-    const testPhone =(value:number) => {
+    const testPhone = (value: number) => {
         setPhone(value)
         testButton()
     }
-    const testMessage =(value:string) => {
+    const testMessage = (value: string) => {
         setMessage(value)
         testButton()
     }
 
-    const testButton = ()=>{
-        if (name.length >= 3){
-            if (email.length >= 11){
-                if(phone >= 10000000000){
-                    if (message.length >= 8){
+    const testButton = () => {
+        if (name.length >= 3) {
+            if (email.length >= 11) {
+                if (phone >= 10000000000) {
+                    if (message.length >= 8) {
                         setEdit(false)
                     }
                 }
             }
-        }
-        else{
+        } else {
             setEdit(true)
         }
     }
-    const sendMessage = ()=>{
+    const sendMessage = () => {
         setEdit(true)
         setName('')
         setEmail('')
@@ -67,18 +67,18 @@ export const Communication = () => {
         alert("your message has been sent")
     }
 
-    function sendEmail  (e: any) {
-            e.preventDefault();
+    function sendEmail(e: any) {
+        e.preventDefault();
 
-            emailjs.sendForm('service_mtj7tps', 'template_zl9m2rl', e.target,
-                'user_MBKEz0Lx85KlXMgE8JZop')
-                .then((result) => {
-                    console.log(result);
-                }, (error) => {
-                    console.log(error.text);
-                });
-            e.target.reset()
-        }
+        emailjs.sendForm('service_mtj7tps', 'template_zl9m2rl', e.target,
+            'user_MBKEz0Lx85KlXMgE8JZop')
+            .then((result) => {
+                console.log(result);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset()
+    }
 
     return (
         <div className={css.communication}>
@@ -86,41 +86,43 @@ export const Communication = () => {
             <form className="contact-form" onSubmit={sendEmail}>
                 <div className={css.communication_table}>
                     <div className='input_wrapper'>
-                        <input key={ContactInput[0].id}  placeholder={ContactInput[0].title}
-                               // onChange={e=>testName(e.currentTarget.value)}value={name}
+                        <input key={ContactInput[0].id} placeholder={ContactInput[0].title}
+                            // onChange={e=>testName(e.currentTarget.value)}value={name}
                                name={'name'}
                                type={'text'}/>
                     </div>
 
-                   <div className='input_wrapper'>
-                       <input key={ContactInput[1].id} placeholder={ContactInput[1].title}
-                              // onChange={e=>testEmail(e.currentTarget.value)} value={email}
-                              name={"email"}
-                              type={"text"}/>
-                   </div>
+                    <div className='input_wrapper'>
+                        <input key={ContactInput[1].id} placeholder={ContactInput[1].title}
+                            // onChange={e=>testEmail(e.currentTarget.value)} value={email}
+                               name={"email"}
+                               type={"text"}/>
+                    </div>
 
                     <div className='input_wrapper'>
                         <input key={ContactInput[2].id} placeholder={ContactInput[2].title}
-                               // onChange={e=>testPhone(+e.currentTarget.value)} value={phone}
+                            // onChange={e=>testPhone(+e.currentTarget.value)} value={phone}
                                name="phone"
                                type="number"/>
                     </div>
 
 
                     <div className='input_wrapper'>
-                        <input key={ContactInput[3].id}  placeholder={ContactInput[3].title}
-                               // onChange={e=>testMessage(e.currentTarget.value)} value={message}
+                        <input key={ContactInput[3].id} placeholder={ContactInput[3].title}
+                            // onChange={e=>testMessage(e.currentTarget.value)} value={message}
                                name="message"
                                type="text"/>
                     </div>
 
-                    <div className={css.button}>
-                        <div className={ edit ? css.buttonOn : css.buttonOff}>
-                            <button disabled={ !edit && edit } onClick={sendMessage} >Send</button>
-                        </div>
+
+                    <div className={edit ? css.buttonOn : css.buttonOff}>
+                        <button disabled={!edit && edit} onClick={sendMessage}>
+                            <span className={css.button}>Send</span>
+                        </button>
                     </div>
                 </div>
             </form>
         </div>
     )
+
 }
